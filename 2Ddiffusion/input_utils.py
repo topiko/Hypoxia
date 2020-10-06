d0c52a70a1e4db3dff54d7eccc89e3e2dd8b7037
topiko <topiko1987@gmail.com>
2020-10-06 09:07:52 +0200

    Take the K_rm param into account also in filenames..

--- 2Ddiffusion/input_utils.py
+++ 2Ddiffusion/input_utils.py
@@ -3,7 +3,7 @@ import argparse
 
 res_dir = 'Data/diffusion_iz={layer_idx:02d}_np={npixels}_activeV={active_vessels:02d}_K0={K_0:g}_D={D:g}_Km={K_m:g}_C0={C_0:g}_rid={run_idx:03d}/'
 
-dose_data = 'Data/D99_doses_layer_idx={layer_idx}_K0={K_0}_D={D}_Km={K_m}_C0={C_0}_alpha={alpha}_beta={beta}_eq={oereq}.npy'
+dose_data = 'Data/D99_doses_layer_idx={layer_idx}_K0={K_0}_D={D}_Km={K_m}_C0={C_0}_alpha={alpha}_beta={beta}_eq={oereq}_Krm={K_rm}.npy'
 
 terminated_flag = 'fin'
 
